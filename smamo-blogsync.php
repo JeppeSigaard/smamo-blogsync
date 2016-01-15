ce7c3e9776ab937a3c677e1878c51ded696778b5
JeppeSigaard <jeppe@smatmonkey.dk>
2016-01-15 12:24:57 +0100

    created plugin header

---
+++ smamo-blogsync.php
@@ -0,0 +1,10 @@
+<?php 
+
+/*
+    Plugin name: Blog Syncer
+    PLugin URI: 
+    Description: Synkroniser indlæg mellem flere WordPress installationer. 
+    Version: 1.0.0
+    Auhtor: SmartMonkey
+    Author URI: http://smartmonkey.dk  
+*/
